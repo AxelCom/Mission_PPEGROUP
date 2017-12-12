af6fe76b93885ed868016e4d2d4ec07c1a511798
COMMERGNAT <AxelCom@users.noreply.github.com>
2017-12-12 12:22:42 +0100

    Test 1

--- commergnat_boutique/Program.cs
+++ commergnat_boutique/Program.cs
@@ -11,6 +11,7 @@ namespace commergnat_boutique
         /// <summary>
         /// Point d'entrée principal de l'application.
         /// </summary>
+        /// // HEllo
         [STAThread]
         static void Main()
         {
