4536cebb5fedcc9973b7722317382720be8f3b6b
Dorian <34375783+DorianSon@users.noreply.github.com>
2017-12-12 12:41:53 +0100

    sdgrft

--- commergnat_boutique/Program.cs
+++ commergnat_boutique/Program.cs
@@ -11,7 +11,7 @@ namespace commergnat_boutique
         /// <summary>
         /// Point d'entrée principal de l'application.
         /// </summary>
-        /// // HEllo word puio
+        /// // HEllo word pusdfdrg
         [STAThread]
         static void Main()
         {
